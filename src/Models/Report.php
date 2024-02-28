32ea0a631f33040bc41177c537610e17b5fe56c6
Mr-Wolf-GB <gaith_baccari@hotmail.fr>
2024-02-28 10:44:55 +0100

    add workflow

--- src/Models/Report.php
+++ src/Models/Report.php
@@ -13,6 +13,14 @@ namespace MrWolfGb\Traccar\Models;
 use Illuminate\Database\Eloquent\Model;
 use MrWolfGb\Traccar\Trait\ArrayToModel;
 
+/**
+ * MrWolfGb\Traccar\Models\Report
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Report query()
+ * @mixin \Eloquent
+ */
 class Report extends Model
 {
     use ArrayToModel;
